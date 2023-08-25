1bcd4e492473fcbba82346830353b3a44cd53728
SumiyaRahman <sumiarahman01739@gmail.com>
2023-08-25 11:56:20 +0600

    not completed

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,35 @@ gPlay.classList.add('play-button');
 
 const aStore = document.getElementById('a-store');
 aStore.classList.add('app-store');
+
+// free price list
+document.getElementById('free-trial').addEventListener('click', function(){
+    alert('Are you willing to choose our free version?');
+});
+document.getElementById('free-trial').addEventListener('click', function(){
+    alert('Thank You');
+});
+
+// business price list
+document.getElementById('business-link').addEventListener('click', function () {
+    alert('If you choose this, you need to add card details for payment.');
+    
+});
+
+// developer price list
+document.getElementById('developer.price').addEventListener('click', function(){
+    alert('Add card details for payment.')
+})
+
+// button send message
+document.getElementById('send-message').classList.add('send-mess-button');
+
+document.getElementById('send-message').addEventListener('click', function(){
+    let inputField = document.getElementsByClassName('show-value');
+    let inputText = inputField.value;
+    
+    let showList = document.getElementById('show-info-list');
+    let p = document.createElement('p');
+    p.innerText = inputText;
+    showList.appendChild('p');
+})
